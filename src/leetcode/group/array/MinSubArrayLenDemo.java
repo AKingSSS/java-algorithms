007908ead11d8a35a26e8e8cba185c3d79c44692
kang.yang <kang.yang@b-and-qchina.com>
2020-06-09 18:04:42 +0800

    数组

---
+++ src/leetcode/group/array/MinSubArrayLenDemo.java
@@ -0,0 +1,20 @@
+package leetcode.group.array;
+
+/**
+ * @ClassName MinSubArrayLenDemo
+ * @Description 209. 长度最小的子数组
+ * @Author yk
+ * @Date 2020/6/9 17:58
+ * @Version 1.0
+ **/
+public class MinSubArrayLenDemo {
+    /**
+     * 给定一个含有 n 个正整数的数组和一个正整数 s ，
+     * 找出该数组中满足其和 ≥ s 的长度最小的连续子数组，并返回其长度。
+     * 如果不存在符合条件的连续子数组，返回 0。
+     *
+     */
+    public int minSubArrayLen(int s, int[] nums) {
+        
+    }
+}
